0c9d028a7e6f2218ffb096db3928b3c9b2ae492c
Hamza Nemmassi <hnemmass>
2026-03-03 22:50:10 +0000

    made the class un-initializable by the user

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: hnemmass <hnemmass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 21:54:03 by hnemmass          #+#    #+#             */
-/*   Updated: 2026/03/03 20:31:56 by hnemmass         ###   ########.fr       */
+/*   Updated: 2026/03/03 22:48:45 by hnemmass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,13 @@
 
 class ScalarConverter
 {
-	public:
+	private:
 		ScalarConverter();
 		ScalarConverter(const ScalarConverter &other);
 		ScalarConverter &operator=(const ScalarConverter &other);
 		~ScalarConverter();
 		
+	public:
 		static void convert(const std::string &s);
 };
 
